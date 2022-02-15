786754f1de57c6cdd02f8880d5fe46c5dfeaf049
NonamedBread <lgh778923@gmail.com>
2022-02-15 18:10:54 +0900

    nftcardTest

---
+++ client/src/components/nftCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import './nftCard.css'
+  
+const Nftcard = () => {
+  return (
+    <div>
+      <Card>
+        <CardBody className="cardContainer">
+          <div className="imgBox">
+            <img
+              src={
+                "https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/JQ_HusUnDX9WANSKsVQP4CzcfV8.jpg"
+              }
+              alt="no img"
+              className="nftImg"
+            />
+          </div>
+          <div>
+            <CardText className="tokenName">Dave Starbelly</CardText>
+          </div>
+
+          <div className="descriptionBox">
+            <CardText className="description">
+              "Friendly OpenSea Creature that enjoys long swims in the ocean."
+            </CardText>
+          </div>
+
+          <div>
+            <CardText></CardText>
+            <div>
+              <CardText className="externalUrl">
+                https://example.com/?token_id=3
+              </CardText>
+            </div>
+          </div>
+          <div className="buttonBox">
+            <Button className="nftButton">Details</Button>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Nftcard;
--- client/src/view/Home.js
+++ client/src/view/Home.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from 'react-router-dom';
+import Nftcard from "../components/nftCard";
 
 // Home 컨탠츠 없음 
 const Home = () => {
@@ -10,7 +11,10 @@ const Home = () => {
         <h1>홈</h1>
         <Link to="profile"> 소개 </Link>
         <p>가장 먼저 보여지는 페이지입니다.</p>
+        <Nftcard></Nftcard>
     </div>
+
+    
   );
 };
 
